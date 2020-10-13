3f4353badead49590cc431e2845a3d27946d6082
torn322 <53220943+torn322@users.noreply.github.com>
2020-10-13 15:13:43 +0700

    added rendering all of the components

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 $(document).ready(function () {
 
     function getJson() {
-        
+
     }
 
 
     $(document).on('input', 'input', function () {
-        const mask = '(999) 99-99-999'
-        const regExpTemplate = mask.replaceAll('(', '').replaceAll(')', '').replaceAll('-', ')(').replaceAll(' ', ')(').replace(/$/, ')').replace(/^/, '(').replaceAll(/[0-9]/g, '[0-9]')
-        let insertTemplate  = mask.replaceAll(/[0-9]+/g, '#')
+        const mask = $(this).data('mask')
+        const regExpTemplate = mask.replaceAll('(', '').replaceAll(')', '').replaceAll('-', ')(').replaceAll(' ', ')(').replace(/$/, ')').replace(/^/, '(').replaceAll(/[0-9]/g, '[0-9]').replace(/\+[0-9]/, '\+[0-9]')
+        console.log(regExpTemplate)
+        let insertTemplate = mask.replaceAll(/[0-9]+/g, '#')
 
         let index = 1
         while (insertTemplate.search('#') + 1) {
             insertTemplate = insertTemplate.replace('#', '$' + index)
             index++
-          }
+        }
 
         const re = new RegExp(regExpTemplate)
 
@@ -29,42 +30,113 @@ $(document).ready(function () {
         },
         getId() {
             return Math.round(Math.random() * 100000000)
-        }, 
-        default(element) {
+        },
+        default (element) {
             const id = this.getId()
-            const html =  
+            const placeholder = element.input.placeholder ? `placeholder="${element.input.placeholder}"` : String()
+            const mask = element.input.mask ? `data-mask="${element.input.mask}"` : String()
+            const pattern = (element.input.type == 'tel') ? 'pattern="[0-9\-]+"' : String()
+            const html =
                 `<div class="form-group">
                     <label for="field-${id}">${element.label}</label>
-                    <input type="${element.input.type}" class="form-control" id="field-${id}" ${this.checkRequired(element)}>
+                    <input type="${element.input.type}" class="form-control" id="field-${id}" ${this.checkRequired(element)} ${placeholder} ${mask} ${pattern}>
                 </div>`
             return html
         },
         file(element) {
             const id = this.getId()
-            const html = 
+            const multiple = element.input.multiple ? 'multiple' : String()
+            const accept = element.input.filetype ? `accept="${element.input.filetype.join(', ')}"` : String()
+            const html =
                 `<div class="form-group">
                     <label for="field-${id}">${element.label}</label>
-                    <input type="${element.input.type}" class="form-control-file" id="field-${id}" ${this.checkRequired(element)}>
+                    <input type="${element.input.type}" class="form-control-file" id="field-${id}" ${accept} ${multiple} ${this.checkRequired(element)}>
                 </div>`
             return html
         },
         textarea(element) {
             const id = this.getId()
-            const html = 
+            const html =
                 `<div class="form-group">
                     <label for="field-${id}">${element.label}</label>
                     <textarea class="form-control" id="field-${id}" rows="3" ${this.checkRequired(element)}></textarea>
                 </div>`
             return html
         },
+        checkbox(element) {
+            const id = this.getId()
+            const checked = (element.input.checked == 'true') ? 'checked' : String()
+            const html =
+                `<div class="form-group form-check">
+                    <input type="${element.input.type}" class="form-check-input" id="field-${id}" ${this.checkRequired(element)} ${checked}>
+                </div>`
+            return html
+        },
 
-        text(e) { return this.default(e) },
-        number(e) { return this.default(e) },
-        email(e) { return this.default(e) },
-        date(e) {return this.default(e) } 
+        number(e) {
+            e.input.type = 'tel'
+            return this.default(e)
+        },
+        text(e) {
+            return this.default(e)
+        },
+        email(e) {
+            return this.default(e)
+        },
+        date(e) {
+            return this.default(e)
+        },
+
+        technology(element) {
+            const id = this.getId()
+            const multiple = element.input.multiple ? 'multiple' : String()
+
+            let options = String()
+            element.input.technologies.forEach(item => {
+                options += `<option>${item}</option>`
+            })
+
+            const html = 
+                `<div class="form-group">
+                    <label for="field-${id}">${element.label}</label>
+                    <select class="form-control" id="field-${id}" ${multiple} ${this.checkRequired(element)}>
+                        ${options}
+                    </select>
+                </div>`
+            
+            return html
+        }
     }
 
-    
+
+
+    const ref = {
+        handle(references) {
+            let id = String()
+            let html = String()
+            references.forEach(element => {
+                if (element.input) {
+                    id = input.getId()
+                    const checked = (element.input.checked == 'true') ? 'checked' : String()
+                    html += `<input class="form-check-input" type="checkbox" id="${id}" ${checked} ${input.checkRequired(element)}>`
+                } else {
+                    html += `<label class="form-check-label" for="${id}">${element['text without ref']}</label>`
+                    html += `<a href="${element.ref}">${element.text}</a>`
+                }
+            })
+            return `<div class="form-check">${html}</div>`
+        }
+    }
+
+    const button = {
+        handle(element) {
+            return `<button type="button" class="btn btn-primary">${element.text}</button>`
+        }
+    }
+
+
+
+
 
 
     $('#btn').click(() => {
@@ -85,9 +157,19 @@ $(document).ready(function () {
                 formHTML += input[element.input.type](element)
             });
 
-            // formJSON.buttons.forEach(element => {
-                
-            // });
+            // formJSON.references.forEach(element => {
+            //     if (element.input) {
+            //         formHTML += input[element.input.type](element)
+            //     } else {
+
+            //     }
+            // })
+
+            formHTML += ref.handle(formJSON.references)
+
+            formJSON.buttons.forEach(element => {
+                formHTML += button.handle(element)
+            });
 
             $('form').html(formHTML)
         }
